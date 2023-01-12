dad0706fbf9fabef751a17bc1b15eca5ed08d4a6
Gabriel Ferreira <gabriel@treiv.com.br>
2023-01-12 04:06:25 -0300

    c

--- services/apiFront.ts
+++ services/apiFront.ts
@@ -14,7 +14,9 @@ export function setupApiClient(ctx = undefined) {
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: "http://localhost:3000/api/",
+        baseURL: process.env.NEXT_PUBLIC_API_URL
+            ? process.env.NEXT_PUBLIC_API_URL
+            : "http://localhost:3000/api/",
         headers: {
             Authorization: `Bearer ${cookies["imo7.token"]}`,
         },
