067b71eb2c0d97cdcce5e42b0b18eeb47f4723d4
Gabriel <gabriel@treiv.com.br>
2023-04-12 01:17:19 -0300

    c

--- pages/api/assunto/index.ts
+++ pages/api/assunto/index.ts
@@ -10,150 +10,34 @@ handle.get(async (req, res) => {
     try {
         const {
             query,
-            codigo,
-            inquilino,
-            dataInicio,
-            vencimento,
-            status,
-            endereco,
-            numero,
-            bairro,
-            cidade,
-            estado,
             pagina,
             linhas,
-            contratoId,
+            departamentoId
         } = req.query;
 
-        let filtroQuery: Prisma.BoletoWhereInput = {};
+        let filtroQuery: Prisma.AssuntoChamadoWhereInput = {};
         if (query) {
             filtroQuery = {
                 ...filtroQuery,
                 OR: [
+                   
                     {
-                        contrato: {
-                            codigo: {
-                                contains: query,
-                            },
-                        },
-                    },
-                    {
-                        data_vencimen: {
+                        titulo: {
                             contains: query,
                         },
                     },
-                    {
-                        contrato: {
-                            inquilinos: {
-                                every: {
-                                    nome: {
-                                        contains: inquilino,
-                                    },
-                                },
-                            },
-                        },
-                    },
+                    
                 ],
             };
         }
-        if (contratoId) {
-            filtroQuery = {
+
+        if(departamentoId){
+            filtroQuery={
                 ...filtroQuery,
-                contrato: {
-                    id: Number(contratoId),
-                },
-            };
+                departamentoId:Number(departamentoId)
+            }
         }
-        if (codigo) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    codigo: {
-                        contains: codigo,
-                    },
-                },
-            };
-        }
-        if (vencimento) {
-            filtroQuery = {
-                ...filtroQuery,
-                data_vencimen: moment(vencimento).format(),
-            };
-        }
-        if (inquilino) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    inquilinos: {
-                        every: {
-                            nome: {
-                                contains: inquilino,
-                            },
-                        },
-                    },
-                },
-            };
-        }
-        if (endereco) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    imovel: {
-                        endereco: {
-                            contains: endereco,
-                        },
-                    },
-                },
-            };
-        }
-        if (numero) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    imovel: {
-                        numero: {
-                            contains: numero,
-                        },
-                    },
-                },
-            };
-        }
-        if (bairro) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    imovel: {
-                        bairro: {
-                            contains: bairro,
-                        },
-                    },
-                },
-            };
-        }
-        if (cidade) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    imovel: {
-                        cidade: {
-                            contains: cidade,
-                        },
-                    },
-                },
-            };
-        }
-        if (estado) {
-            filtroQuery = {
-                ...filtroQuery,
-                contrato: {
-                    imovel: {
-                        estado: {
-                            contains: estado,
-                        },
-                    },
-                },
-            };
-        }
+        
         let paginacao = {};
         if (pagina && linhas) {
             paginacao = {
