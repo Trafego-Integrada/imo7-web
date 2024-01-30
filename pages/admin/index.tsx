ddd7c21ba868a992d85f93a03b2c72f2f2e4737f
Izequias Silva <master@dedicado.digital>
2024-01-30 14:08:02 -0300

    Otimizada as importações e melhoria no tratamento de formulários da página da conta do usuário.
    Limpeza e simplificação das importações em diversas páginas de administração, melhorando
    legibilidade e manutenção do código. Em 'minha-conta', refinada a forma de processamento usando o gancho `useMutation` para busca de dados de usuário, atualizações de perfil, integrando melhor gerenciamento de estado e tratamento de erros.
    Essas mudanças resultam em uma experiência de usuário mais tranquila ao interagir com
    as configurações da conta, bem como uma base de código mais eficiente, propícia para
    futuras expansões ou modificações.

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,57 +1,27 @@
-import {
-    Box,
-    Button,
-    Collapse,
-    Flex,
-    Grid,
-    GridItem,
-    Icon,
-    IconButton,
-    Table,
-    Tbody,
-    Td,
-    Text,
-    Th,
-    Thead,
-    Tr,
-    useDisclosure,
-} from "@chakra-ui/react";
-import React, { useRef } from "react";
-import { IoIosRemoveCircle } from "react-icons/io";
-import {
-    MdOutlineKeyboardArrowDown,
-    MdOutlineKeyboardArrowUp,
-    MdPageview,
-} from "react-icons/md";
-import { FormDate } from "@/components/Form/FormDate";
-import { FormInput } from "@/components/Form/FormInput";
-import { FormSelect } from "@/components/Form/FormSelect";
-import { Layout } from "@/components/Layout/layout";
-import { ModalContratos } from "@/components/Modals/contratos";
-import { withSSRAuth } from "@/utils/withSSRAuth";
-import { useQuery } from "react-query";
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { Layout } from '@/components/Layout/layout'
+import { withSSRAuth } from '@/utils/withSSRAuth'
+import { InferGetServerSidePropsType } from "next";
 
-const Home = () => {
-    const { isOpen, onToggle } = useDisclosure();
-    const modalcontratos = useRef();
-
+const Home = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return (
         <>
             <Layout title="Contratos">
                 <Box p={5}></Box>
             </Layout>
         </>
-    );
-};
-export default Home;
+    )
+}
+export default Home
 
 export const getServerSideProps = withSSRAuth(
     async (ctx) => {
         return {
             props: {},
-        };
+        }
     },
     {
-        cargos: ["imobiliaria", "adm", "conta"],
-    }
-);
+        cargos: ['imobiliaria', 'adm', 'conta'],
+    },
+)
