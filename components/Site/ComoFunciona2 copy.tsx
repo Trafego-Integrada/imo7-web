ad8de60057494e10f4be96972395f2f3f01cc715
ElzioLimaTrafegoSolucoesIntegradas <elzio@clientestrafego.com.br>
2024-02-21 16:02:06 -0400

    fix: correcting autoFullScreen iframe attribute

--- components/Site/ComoFunciona2 copy.tsx
+++ components/Site/ComoFunciona2 copy.tsx
@@ -53,7 +53,7 @@ export const ComoFunciona3 = ({ modal }) => {
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen
+                        allowFullScreen
                     ></iframe>
                 </GridItem>
             </Grid>
