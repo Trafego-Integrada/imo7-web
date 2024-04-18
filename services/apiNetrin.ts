3618fa1f344b9c0de05339d7efffdd70540d9c9b
“Natan <natanfoleto2015@hotmail.com>
2024-04-18 00:03:50 -0300

    Corrgindo sintaxe apiNetrin.ts

--- services/apiNetrin.ts
+++ services/apiNetrin.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios'
 
 function setupApiClient() {
     const api = axios.create({
@@ -12,14 +12,13 @@ export const apiNetrin = setupApiClient()
 
 export const apiNetrinService = () => ({
     consultaComposta: async (consulta: any) => {
-    consultaComposta: async (consulta: any) => {
         const { data } = await apiNetrin.get(`consulta-composta`, {
             params: {
                 token: NETRIN_API_TOKEN,
                 ...consulta,
             },
-        });
+        })
 
-        return data;
+        return data
     },
 })
