43eaaef3c695b5cc93ae944fa93ace7e0b4cec64
Daniel Iseri <danielsi-2015@hotmail.com>
2024-06-05 14:56:32 -0300

    Adicionar Link

--- components/RodapeFicha.tsx
+++ components/RodapeFicha.tsx
@@ -1,10 +1,12 @@
-import { Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Image, Link, Text } from "@chakra-ui/react";
 
 export function RodapeFicha(){
     return (
         <Grid>
-            <GridItem display='flex' alignItems='center' justifyContent='center'>
-                <Text>Plataforma Produzido e gerenciada por:</Text> <Image width={24} src="/img/LOGO-IMO7-FUNDO-CLARO 1.png" />
+            <GridItem>
+                <Link target="_blank" display='flex' alignItems='center' justifyContent='center' href="https://www.imo7.com.br">
+                    <Text>Plataforma Produzido e gerenciada por:</Text>  <Image width={20} src="/img/LOGO-IMO7-FUNDO-CLARO 1.png" />
+                </Link>
             </GridItem>
         </Grid>
     )
