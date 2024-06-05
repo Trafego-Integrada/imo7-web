6a5a630094951bbf07d3342b6a3dcab6a2925672
Daniel Iseri <danielsi-2015@hotmail.com>
2024-06-05 14:49:38 -0300

    Align Footer

--- components/RodapeFicha.tsx
+++ components/RodapeFicha.tsx
@@ -3,8 +3,8 @@ import { Grid, GridItem, Image, Text } from "@chakra-ui/react";
 export function RodapeFicha(){
     return (
         <Grid>
-            <GridItem display='flex' alignItems='center' justifyContent='end'>
-            <Text>Plataforma Produzido e gerenciada por:</Text> <Image width={24} src="/img/LOGO-IMO7-FUNDO-CLARO 1.png" />
+            <GridItem display='flex' alignItems='center' justifyContent='center'>
+                <Text>Plataforma Produzido e gerenciada por:</Text> <Image width={24} src="/img/LOGO-IMO7-FUNDO-CLARO 1.png" />
             </GridItem>
         </Grid>
     )
