20a781fd6a3ec1eba87c848093760c97fd488ca2
“Natan <natanfoleto2015@hotmail.com>
2024-06-26 14:59:48 -0300

    Button disable withou photo

--- pages/_sites/[site]/validacao-facial/[id].tsx
+++ pages/_sites/[site]/validacao-facial/[id].tsx
@@ -56,50 +56,50 @@ import { FiArrowLeft, FiArrowRight, FiCheck } from 'react-icons/fi'
  *	Copyright (c) 2014-2015 Tiago Porto (http://www.tiagoporto.com)
  *	Released under the MIT license
  */
-function CPF() {
-    'user_strict'
-    function r(r) {
-        for (var t = null, n = 0; 9 > n; ++n)
-            t += r.toString().charAt(n) * (10 - n)
-        var i = t % 11
-        return (i = 2 > i ? 0 : 11 - i)
-    }
-    function t(r) {
-        for (var t = null, n = 0; 10 > n; ++n)
-            t += r.toString().charAt(n) * (11 - n)
-        var i = t % 11
-        return (i = 2 > i ? 0 : 11 - i)
-    }
-    var n = 'CPF Inválido',
-        i = 'CPF Válido'
-        ; (this.gera = function () {
-            for (var n = '', i = 0; 9 > i; ++i)
-                n += Math.floor(9 * Math.random()) + ''
-            var o = r(n),
-                a = n + '-' + o + t(n + '' + o)
-            return a
-        }),
-            (this.valida = function (o) {
-                for (
-                    var a = o.replace(/\D/g, ''),
-                    u = a.substring(0, 9),
-                    f = a.substring(9, 11),
-                    v = 0;
-                    10 > v;
-                    v++
-                )
-                    if (
-                        '' + u + f ==
-                        '' + v + v + v + v + v + v + v + v + v + v + v
-                    )
-                        return n
-                var c = r(u),
-                    e = t(u + '' + c)
-                return f.toString() === c.toString() + e.toString() ? i : n
-            })
-}
+// function CPF() {
+//     'user_strict'
+//     function r(r) {
+//         for (var t = null, n = 0; 9 > n; ++n)
+//             t += r.toString().charAt(n) * (10 - n)
+//         var i = t % 11
+//         return (i = 2 > i ? 0 : 11 - i)
+//     }
+//     function t(r) {
+//         for (var t = null, n = 0; 10 > n; ++n)
+//             t += r.toString().charAt(n) * (11 - n)
+//         var i = t % 11
+//         return (i = 2 > i ? 0 : 11 - i)
+//     }
+//     var n = 'CPF Inválido',
+//         i = 'CPF Válido'
+//         ; (this.gera = function () {
+//             for (var n = '', i = 0; 9 > i; ++i)
+//                 n += Math.floor(9 * Math.random()) + ''
+//             var o = r(n),
+//                 a = n + '-' + o + t(n + '' + o)
+//             return a
+//         }),
+//             (this.valida = function (o) {
+//                 for (
+//                     var a = o.replace(/\D/g, ''),
+//                     u = a.substring(0, 9),
+//                     f = a.substring(9, 11),
+//                     v = 0;
+//                     10 > v;
+//                     v++
+//                 )
+//                     if (
+//                         '' + u + f ==
+//                         '' + v + v + v + v + v + v + v + v + v + v + v
+//                     )
+//                         return n
+//                 var c = r(u),
+//                     e = t(u + '' + c)
+//                 return f.toString() === c.toString() + e.toString() ? i : n
+//             })
+// }
 
-function cpfMask(v) {
+function cpfMask(v: any) {
     if (typeof v === 'undefined') return
     v = v.replace(/\D/g, '') //Remove tudo o que não é dígito
     v = v.replace(/(\d{3})(\d)/, '$1.$2') //Coloca um ponto entre o terceiro e o quarto dígitos
@@ -303,7 +303,6 @@ const ValidacaoFacial: NextPage = ({ imobiliaria, validacao }: any) => {
             </Flex>
         )
 
-
     return (
         <>
             {(JSON.parse(validacao?.resultado)?.token) ? (
@@ -470,6 +469,7 @@ const ValidacaoFacial: NextPage = ({ imobiliaria, validacao }: any) => {
                                                     ></div>
                                                 </>
                                             )}
+
                                             {photo != null && (
                                                 <div>
                                                     <ChakraImage
@@ -624,7 +624,7 @@ const ValidacaoFacial: NextPage = ({ imobiliaria, validacao }: any) => {
                                     size="sm"
                                     type="submit"
                                     isLoading={isSubmitting}
-                                    disabled={activeStep != 3}
+                                    isDisabled={!photo}
                                     onClick={handleSubmit(onSubmit)}
                                 >
                                     Continuar
